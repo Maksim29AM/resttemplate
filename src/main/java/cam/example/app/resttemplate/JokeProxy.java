8501f3cb209394599c27e01a46f4ca7338f916a8
Maksim29AM <maks.abramovich.1995@mail.ru>
2025-09-05 20:14:40 +0200

    Refactor JokeProxy to add error handling for API requests

--- src/main/java/cam/example/app/resttemplate/JokeProxy.java
+++ src/main/java/cam/example/app/resttemplate/JokeProxy.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
 @Component
@@ -19,16 +21,25 @@ public class JokeProxy {
     @Value("${joke.service.url}")
     String url;
 
-    public JokeResponse makeJokeRequest(String category) throws JsonProcessingException {
+    public String makeJokeRequest(String category) throws JsonProcessingException {
         String uri = url + "/joke/" + category;
-        ResponseEntity<String> response = restTemplate.exchange(
-                uri,
-                HttpMethod.GET,
-                null,
-                String.class
-        );
-        String json = response.getBody();
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(json, JokeResponse.class);
+        return makeRequest(uri);
+    }
+
+    private String makeRequest(String uri) {
+        try {
+            ResponseEntity<String> response = restTemplate.exchange(
+                    uri,
+                    HttpMethod.GET,
+                    null,
+                    String.class
+            );
+            return response.getBody();
+        } catch (RestClientResponseException exceptions) {
+            System.out.println(exceptions.getStatusText() + " " + exceptions.getStatusCode().value());
+        } catch (RestClientException exception) {
+            System.out.println(exception.getMessage());
+        }
+        return null;
     }
 }
